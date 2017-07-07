abb5577d417b4cfb9ec28de6d1122136ba6d0673
♫ JIA YEE ♚ <jiayeerawr@gmail.com>
2017-07-07 17:05:11 +0800

    Remove ping test route
    Signed-off-by: ♫ JIA YEE ♚ <jiayeerawr@gmail.com>

--- controller.py
+++ controller.py
@@ -13,19 +13,6 @@ def connect():
     data = request.get_json()
     return emeow(app, data)
 
-@app.route('/ping', methods = ('GET', ))
-def ping():
-    emeow(
-        app,
-        {
-          "enquiry":"9",
-          "name":"jiayee",
-          "email":"jia10@u.nus.edu",
-          "message":"Help!"
-        }
-    )
-    return 'OK'
-
 @app.after_request
 def set_headers(resp):
   resp.headers['Access-Control-Allow-Headers'] = 'Content-Type'
